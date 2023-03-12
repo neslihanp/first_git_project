297cad8114f0641f7d9b97fc5be9cfaad3c40d84
Neslihan Parlak <nslhn24@gmail.com>
2023-03-11 22:00:12 -0800

    I commiting another class

---
+++ src/main/java/first_git_package/I.java
@@ -0,0 +1,10 @@
+package first_git_package;
+
+public class I {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am checking the steps" +
+                "every steps");
+    }
+}
